f20e146f44e84a18106729e9438da1e1fbb15436
MWildFire <mvstrike17@gmail.com>
2021-02-07 15:28:55 +0300

    Deleted unnesecesary imports

--- AllInOne/__init__.py
+++ AllInOne/__init__.py
@@ -16,10 +16,6 @@ def create_app():
 
     db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
     from .auth import auth as auth_blueprint
 
     app.register_blueprint(auth_blueprint)
@@ -28,10 +24,5 @@ def create_app():
 
     app.register_blueprint(main_blueprint)
 
-    from .models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
     return app
